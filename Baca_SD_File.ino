41cf545f265de04609b2ac6c2a6c7af7ab0b7111
Hollanda Arief Kusuma <hollandacocobear@gmail.com>
2022-05-17 23:24:57 +0700

    Create Baca_SD_File.ino

---
+++ Baca_SD_File.ino
@@ -0,0 +1,46 @@
+#include <SPI.h>
+#include <SD.h>
+
+File myFile;
+String namafile="/LOG0022.TXT";
+byte SD_CS_pin = 4;
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(SD_CS_pin)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = SD.open(namafile);
+
+  if (myFile) {
+    Serial.println(namafile);
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.print("error opening ");
+    Serial.println(namafile);
+  }
+}
+
+void loop() {
+  // nothing happens after setup
+}
